958baa33d7859a25557830c29513321e3efa77d1
mahsagn <mahsa.ghaderan@outlook.com>
2019-10-20 18:44:09 +0330

    Add methon to fetch data for show palce

--- src/home.js
+++ src/home.js
@@ -3,26 +3,27 @@ import 'bootstrap/dist/css/bootstrap.css';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import './style.css';
 import Place from './Place/Place'
+import axios from 'axios'
  export default class home extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
           palceData:[],
         };
-    
+        this.fetchPlace=this.fetchPlace.bind(this)
       }
-    
-      componentDidMount() {
-        console.log("in mount")
-            fetch('  http://localhost:3000/places/1').
-            then(response =>response.json()).
-            then(d => {
-                this.setState({
-                    palceData: d
-                })
-                console.log(d)
-            });
-    
+      //id ro too url behesh bedam???
+      fetchPlace(e) {
+        console.log("in handel submit")
+        e.preventDefault();
+        axios.get('http://localhost:8000/api/token/')
+        .then(json => {
+          console.log("response")
+          console.log(json)
+          this.setState({palceData: json})
+        }).catch(error =>{
+        alert("اشتباه رخ داده ست")
+      });
     }
     handleClick(){
       return window.location.replace('/place/1')
@@ -33,7 +34,7 @@ import Place from './Place/Place'
         return (
           <div >
             <p onClick={this.handleClick}>hiiii clock on me</p>
-              <Link to={{pathname: `/place/${this.state.palceData.id}`,
+              <Link onClick={this.fetchPlace} to={{pathname: `/place/${this.state.palceData.id}`,
             data : {info: this.state.palceData}
         }}>click to see first plasey</Link>
           </div>
