9a52bc5c226599238a1d4e21a8aa6098bf4f0053
fatemerg <47068576+fatemerg@users.noreply.github.com>
2019-10-25 22:58:09 +0330

    login and signup button added

--- src/homePage.js
+++ src/homePage.js
@@ -7,8 +7,11 @@ import { CardImg, CardBody, CardTitle, CardSubtitle, Button,ButtonGroup} from 'r
 
 const homePage = (props) => {
   return (
-    <div >
-     
+    <div align="right">
+      <ButtonGroup id="bg">
+        <Button  className="loginSignupButton">Login</Button> 
+        <Button  className="loginSignupButton">Signup</Button>
+      </ButtonGroup>
       <div class="row">
         <div class="column">
           <CardImg  src={place}/>
@@ -31,6 +34,58 @@ const homePage = (props) => {
           <CardBody>
             <CardTitle >place3</CardTitle>
             <CardSubtitle className="cardsub">this is the place3</CardSubtitle>
+            <Button className="selectButton">select</Button>
+          </CardBody>
+          </div> 
+      </div>
+      <div class="row">
+        <div class="column">
+          <CardImg  src={place}/>
+          <CardBody >
+            <CardTitle >place1</CardTitle>
+            <CardSubtitle className="cardsub">this is the place1</CardSubtitle>
+            <Button className="selectButton">select</Button>
+          </CardBody>
+        </div>
+        <div class="column">
+          <CardImg  src={place} />
+          <CardBody>
+            <CardTitle >place2</CardTitle>
+            <CardSubtitle className="cardsub">this is the place2</CardSubtitle>
+            <Button className="selectButton">select</Button>
+          </CardBody>
+         </div>
+         <div class="column">
+          <CardImg  src={place} />
+          <CardBody>
+            <CardTitle >place3</CardTitle>
+            <CardSubtitle className="cardsub">this is the place3</CardSubtitle>
+            <Button className="selectButton">select</Button>
+          </CardBody>
+          </div> 
+      </div>
+      <div class="row">
+        <div class="column">
+          <CardImg  src={leader}/>
+          <CardBody >
+            <CardTitle >leader1</CardTitle>
+            <CardSubtitle className="cardsub">this is the leader1</CardSubtitle>
+            <Button className="selectButton">select</Button>
+          </CardBody>
+        </div>
+        <div class="column">
+          <CardImg  src={leader} />
+          <CardBody>
+            <CardTitle >leader2</CardTitle>
+            <CardSubtitle className="cardsub">this is the leader2</CardSubtitle>
+            <Button className="selectButton">select</Button>
+          </CardBody>
+         </div>
+         <div class="column">
+          <CardImg  src={leader} />
+          <CardBody>
+            <CardTitle >leader3</CardTitle>
+            <CardSubtitle className="cardsub">this is the leader3</CardSubtitle>
             <Button className="selectButton">select</Button>
           </CardBody>
           </div> 
