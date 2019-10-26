66699c7990e84c9181f6767a30ca644678264b14
mahsagn <mahsa.ghaderan@outlook.com>
2019-10-26 19:40:10 +0330

    Specify loading profile of leader and imporve design

--- src/profile_item.js
+++ src/profile_item.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import './leaderProfile.css'
 import { ListGroup, ListGroupItem } from 'reactstrap';
 
 const profile_item=(props)=>{
-    const ifHasvsl= props.val? <ListGroupItem>{props.title} : {props.val}</ListGroupItem>:null
+    const ifHasvsl= props.val?
+    <div>
+        <ListGroupItem id="tab_content_item_nam">{props.title}</ListGroupItem>
+        <ListGroupItem id="tab_content_item_val"> {props.val}</ListGroupItem>
+    </div> 
+    :null
     return(
             <dev>
                 {ifHasvsl}
