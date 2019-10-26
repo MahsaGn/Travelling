86e3d4777e0e9f46be6b13ff33d16f612dcf048c
fatemerg <47068576+fatemerg@users.noreply.github.com>
2019-10-26 23:51:42 +0330

    class column is made

--- src/homePage.js
+++ src/homePage.js
@@ -1,97 +1,65 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import place from './1.jpg';
-import leader from './2.jpg';
 import './homePage.css';
-import { CardImg, CardBody, CardTitle, CardSubtitle, Button,ButtonGroup} from 'reactstrap';
+import Column from './column';
+import { Button ,Nav, NavLink, Navbar, Form, NavbarBrand } from 'reactstrap';
 
-const homePage = (props) => {
-  return (
-    <div align="right">
-      <ButtonGroup id="bg">
-        <Button  className="loginSignupButton">Login</Button> 
-        <Button  className="loginSignupButton">Signup</Button>
-      </ButtonGroup>
-      <div class="row">
-        <div class="column">
-          <CardImg  src={place}/>
-          <CardBody >
-            <CardTitle >place1</CardTitle>
-            <CardSubtitle className="cardsub">this is the place1</CardSubtitle>
-            <Button className="selectButton">select</Button>
-          </CardBody>
+class homePage extends React.Component {
+  constructor(){
+    super();
+    this.state={topPlaces:[]}
+  };
+  
+  componentWillMount(){
+    console.log("in component")
+    var data ={
+      "p":[{
+      "image" : 'https://www.alaedin.travel/Files/Jazebe/Marvdasht/persepolis/Alaedin-Travel-Company-Attraction-Persepolis-Shiraz-23.jpg',
+      "title" : 'takhtjamshid' 
+      },
+      {
+        "image" : 'https://www.alaedin.travel/Files/Jazebe/Marvdasht/persepolis/Alaedin-Travel-Company-Attraction-Persepolis-Shiraz-23.jpg',
+        "title" : 'takhtjamshid' 
+      },
+      {
+        "image" : 'https://www.alaedin.travel/Files/Jazebe/Marvdasht/persepolis/Alaedin-Travel-Company-Attraction-Persepolis-Shiraz-23.jpg',
+        "title" : 'takhtjamshid'
+      }
+      ]}
+      var topPlacesData = data.p.map((topData)=> <Column info={topData}/>); 
+      this.setState({
+      topPlaces:topPlacesData
+      })
+  }
+
+  render(){
+    
+      
+    return (
+      <div >
+        <Navbar >
+          <NavbarBrand href="#home" id="navbrand">Home</NavbarBrand>
+          <Form inline>
+            <Nav >
+              <NavLink href="#ContactUs" className="navlink">Contact Us</NavLink>
+              <NavLink href="#AboutUs" className="navlink">About Us</NavLink>
+              <NavLink href="#BecomeLeader" className="navlink">BecomeLeader</NavLink>
+              <NavLink href="#AddPlace" className="navlink">Add Place</NavLink>
+            </Nav>
+            <Button variant="outline-primary" className="navbarbutton">Search</Button>
+            <Button variant="outline-primary" className="navbarbutton">Login</Button>
+            <Button variant="outline-primary" className="navbarbutton">Signup</Button>
+          </Form>
+        </Navbar>
+        <img id="homePic" src={'https://tourkadeh.net/source/matalebnew/shiraz/jamshid2/jamshid.jpg'}/>
+  
+        <div class="row">
+          {this.state.topPlaces}
         </div>
-        <div class="column">
-          <CardImg  src={place} />
-          <CardBody>
-            <CardTitle >place2</CardTitle>
-            <CardSubtitle className="cardsub">this is the place2</CardSubtitle>
-            <Button className="selectButton">select</Button>
-          </CardBody>
-         </div>
-         <div class="column">
-          <CardImg  src={place} />
-          <CardBody>
-            <CardTitle >place3</CardTitle>
-            <CardSubtitle className="cardsub">this is the place3</CardSubtitle>
-            <Button className="selectButton">select</Button>
-          </CardBody>
-          </div> 
-      </div>
-      <div class="row">
-        <div class="column">
-          <CardImg  src={place}/>
-          <CardBody >
-            <CardTitle >place1</CardTitle>
-            <CardSubtitle className="cardsub">this is the place1</CardSubtitle>
-            <Button className="selectButton">select</Button>
-          </CardBody>
+        <div class="row">
         </div>
-        <div class="column">
-          <CardImg  src={place} />
-          <CardBody>
-            <CardTitle >place2</CardTitle>
-            <CardSubtitle className="cardsub">this is the place2</CardSubtitle>
-            <Button className="selectButton">select</Button>
-          </CardBody>
-         </div>
-         <div class="column">
-          <CardImg  src={place} />
-          <CardBody>
-            <CardTitle >place3</CardTitle>
-            <CardSubtitle className="cardsub">this is the place3</CardSubtitle>
-            <Button className="selectButton">select</Button>
-          </CardBody>
-          </div> 
       </div>
-      <div class="row">
-        <div class="column">
-          <CardImg  src={leader}/>
-          <CardBody >
-            <CardTitle >leader1</CardTitle>
-            <CardSubtitle className="cardsub">this is the leader1</CardSubtitle>
-            <Button className="selectButton">select</Button>
-          </CardBody>
-        </div>
-        <div class="column">
-          <CardImg  src={leader} />
-          <CardBody>
-            <CardTitle >leader2</CardTitle>
-            <CardSubtitle className="cardsub">this is the leader2</CardSubtitle>
-            <Button className="selectButton">select</Button>
-          </CardBody>
-         </div>
-         <div class="column">
-          <CardImg  src={leader} />
-          <CardBody>
-            <CardTitle >leader3</CardTitle>
-            <CardSubtitle className="cardsub">this is the leader3</CardSubtitle>
-            <Button className="selectButton">select</Button>
-          </CardBody>
-          </div> 
-      </div>
-    </div>
-  );
+    );
+  };
 };
-
-export default homePage;
+export default  homePage;
