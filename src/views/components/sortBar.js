ef4e9bb29587717c6253aec6119dd5392ea0b889
mahsagn <mahsa.ghaderan@outlook.com>
2019-11-08 17:17:42 +0330

    Seperate sortbar from sort page

--- src/views/components/sortBar.js
+++ src/views/components/sortBar.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import { Nav, NavItem, NavLink} from 'reactstrap';
 import classnames from 'classnames';
 
-const Example = (props) => {
-  const [activeTab, setActiveTab] = useState('1');
-
-  const toggle = tab => {
-    if(activeTab !== tab) setActiveTab(tab);
+class sortBar extends React.Component {
+  constructor(props){
+    super(props)
   }
         // <label id="sort_label">مرتب سازی براساس</label>
         // <ButtonGroup id="sort_button_group">
@@ -17,74 +15,60 @@ const Example = (props) => {
         //     <Button id="sort_button">سخت ترین</Button>
         //     <Button id="sort_button">کوتاه ترین زمان بازدید</Button>
         // </ButtonGroup>
+        render(){
   return (
     <div id="sorting_option">
       <Nav tabs>
         <NavItem>
           <NavLink
-            className={classnames({ active: activeTab === '1' })}
-            onClick={() => { toggle('1'); }}
+            className={classnames({ active: this.props.activeTab === '1' })}
+            onClick={() => { this.props.toggle('1'); }}
           >
             محبوب ترین
           </NavLink>
         </NavItem>
         <NavItem>
           <NavLink
-            className={classnames({ active: activeTab === '2' })}
-            onClick={() => { toggle('2'); }}
+            className={classnames({ active: this.props.activeTab === '2' })}
+            onClick={() => { this.props.toggle('2'); }}
           >
             زودترین زمان شروع
           </NavLink>
         </NavItem>
         <NavLink
-            className={classnames({ active: activeTab === '3' })}
-            onClick={() => { toggle('3'); }}
+            className={classnames({ active: this.props.activeTab === '3' })}
+            onClick={() => { this.props.toggle('3'); }}
           >
             محبوب ترین
           </NavLink>
           <NavLink
-            className={classnames({ active: activeTab === '4' })}
-            onClick={() => { toggle('4'); }}
+            className={classnames({ active: this.props.activeTab === '4' })}
+            onClick={() => { this.props.toggle('4'); }}
           >
               دیرترین زمان پایان
           </NavLink>
           <NavLink
-            className={classnames({ active: activeTab === '5' })}
-            onClick={() => { toggle('5'); }}
+            className={classnames({ active: this.props.activeTab === '5' })}
+            onClick={() => { this.props.toggle('5'); }}
           >
             آسان ترین
           </NavLink>
           <NavLink
-            className={classnames({ active: activeTab === '6' })}
-            onClick={() => { toggle('6'); }}
+            className={classnames({ active: this.props.activeTab === '6' })}
+            onClick={() => { this.props.toggle('6'); }}
           >
         سخت ترین
           </NavLink>
           <NavLink
-            className={classnames({ active: activeTab === '7' })}
-            onClick={() => { toggle('7'); }}
+            className={classnames({ active: this.props.activeTab === '7' })}
+            onClick={() => { this.props.toggle('7'); }}
           >
         کوتاه ترین زمان بازدید
           </NavLink>
       </Nav>
-      <TabContent activeTab={activeTab}>
-        <TabPane id="sort_option" tabId="1">
-        </TabPane>
-        <TabPane id="sort_option" tabId="2">
-        </TabPane>
-        <TabPane id="sort_option" tabId="3">
-        </TabPane>
-        <TabPane id="sort_option" tabId="4">
-        </TabPane>
-        <TabPane id="sort_option" tabId="5">
-        </TabPane>
-        <TabPane id="sort_option" tabId="6">
-        </TabPane>
-        <TabPane id="sort_option" tabId="7">
-        </TabPane>
-      </TabContent>
+      
     </div>
-  );
+  )}
 }
 
-export default Example;
+export default sortBar;
--- src/views/pages/sortPlace.js
+++ src/views/pages/sortPlace.js
@@ -2,12 +2,24 @@ import React from 'react'
 import PlaceCard from '../components/placeCard'
 import {connect} from 'react-redux'
 import Header from '../components/header'
+import { TabContent, TabPane } from 'reactstrap';
 import SortPlaceBar from '../components/sortBar'
 import * as sortPlaceAction  from '../../core/sortPlace/sortPlace_action';
 class sortPlace extends React.Component{
-    state={
-        info:""
-    }
+    constructor(){
+        super();
+      this.state = {
+       activeTab: '1',
+       info:""
+     };
+   }
+    toggle = (tab) => {
+       if (this.state.activeTab !== tab) {
+         this.setState({
+           activeTab: tab
+         });
+       }
+   }
     async componentWillMount(){
         var sortedOptoin = window.location.pathname.split('/')[2]
         console.log("in sortedplace",sortedOptoin)
@@ -25,12 +37,28 @@ class sortPlace extends React.Component{
         }
         console.log("after await",this.props.info)
     }
+       
     render(){
-        
         return(
             <div>
                 <Header/>
-                <SortPlaceBar/>
+                <SortPlaceBar activeTab ={this.state.activeTab} toggle={this.toggle}/>
+                <TabContent activeTab={this.state.activeTab}>
+                    <TabPane id="sort_option" tabId="1">
+                    </TabPane>
+                    <TabPane id="sort_option" tabId="2">
+                    </TabPane>
+                    <TabPane id="sort_option" tabId="3">
+                    </TabPane>
+                    <TabPane id="sort_option" tabId="4">
+                    </TabPane>
+                    <TabPane id="sort_option" tabId="5">
+                    </TabPane>
+                    <TabPane id="sort_option" tabId="6">
+                    </TabPane>
+                    <TabPane id="sort_option" tabId="7">
+                    </TabPane>
+                </TabContent>
                 {this.state.info}
             </div>
         )
