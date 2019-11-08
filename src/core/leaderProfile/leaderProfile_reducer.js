14c9afdfb8554e4291cac7d0ddb6e557d95a422f
mahsagn <mahsa.ghaderan@outlook.com>
2019-11-08 15:44:40 +0330

    Add reducer and action

---
+++ src/core/leaderProfile/leaderProfile_reducer.js
@@ -0,0 +1,24 @@
+
+const initialState = {
+    has_profileInfo:false,
+    profile_info:""
+}
+
+export const leaderProfile_reducer = (state = initialState , action) => {
+    console.log("inleaderProfile reducer",action)
+    switch(action.type){
+        case "GETPROFILE_SUCCESS":
+            return {
+                ...state,
+                has_profileInfo:true,
+                profile_info:action.data
+            }
+        case "GETPROFILE_FAILURE":
+            return {
+                ...state,
+                has_profileInfo:false
+            }
+        default:
+            return state
+    }
+}
