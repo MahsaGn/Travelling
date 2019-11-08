859bc85411837e741e3f51bd11b787a55180a852
mahsagn <mahsa.ghaderan@outlook.com>
2019-11-08 16:20:11 +0330

    Show empty image if user dont have image Connect properly to back

--- src/views/pages/userProfile.js
+++ src/views/pages/userProfile.js
@@ -32,7 +32,7 @@ class userProfile extends React.Component{
     render(){
         return(
             <dev id="profilePage">
-                <img id="profileImage" src={this.props.image} />
+                <img id="profileImage" src={this.props.image? this.props.image : this.state.image} />
                 <br/>
                 <h1 id="h1">{this.props.username}</h1>
                 <ListGroup id="items">
