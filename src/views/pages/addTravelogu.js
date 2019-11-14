ee540c519523572b2eadea79682d6e2df4d74874
mahsagn <mahsa.ghaderan@outlook.com>
2019-11-14 16:53:43 +0330

    Add multiple chooice for choosed place of a travelogu

--- src/views/pages/addTravelogu.js
+++ src/views/pages/addTravelogu.js
@@ -5,7 +5,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/login_signupForm.css'
 import * as addTraveloguAction from '../../core/addTravelogu/addTravelogu_action'
 import * as allPlaces from '../../core/allPlaces/allPlaces_action'
-
+import "@kenshooui/react-multi-select/dist/style.css"
+import MultiSelect from "@kenshooui/react-multi-select";
 class addTravelogu extends React.Component{
     constructor(props){
       super(props);
@@ -17,25 +18,25 @@ class addTravelogu extends React.Component{
             image3:"",
             choosedPlaces:[]
         },
-        places:[],
-        currentPlaceList:""
+        places:[]
     };
       this.handleSubmit=this.handleSubmit.bind(this);
       this.handleChange = this.handleChange.bind(this);
-      this.updateStateImage=this.updateStateImage.bind(this)    
+      this.handleChangeMultiChoice = this.handleChangeMultiChoice.bind(this);
+      this.updateStateImage=this.updateStateImage.bind(this);
     }
 
     async componentWillMount(){
         await this.props.allPlace()
         if(this.props.allPlaceLoaded && this.props.allPlaceInfo!=undefined)
         {
-            let options = this.props.allPlaceInfo.map((place)=><option>{place.id}.{place.title}</option>)
+            let options =[]
+            this.props.allPlaceInfo.map((place)=>{
+              let val = { id: place.id, label: place.title }
+              options.push(val)
+               })
             this.setState({
-                    places:options,
-                    choosedPlace:{
-                        title:this.props.allPlaceInfo[0].title,
-                        id:this.props.allPlaceInfo[0].id
-                    }
+                    places:options
                 })
         }
         else{
@@ -58,9 +59,15 @@ class addTravelogu extends React.Component{
         curentSt[e.target.name] = e.target.value
         this.setState({travelogu_info:curentSt})
     }
+    handleChangeMultiChoice(selectedItems) {
+      let curentSt = this.state.travelogu_info
+        curentSt["choosedPlaces"] = selectedItems
+        this.setState({travelogu_info:curentSt})
+        console.log(selectedItems)
+
+    }
 
     updateStateImage(e){
-        console.log(e.target.name)
         let curentSt = this.state.travelogu_info
         curentSt[e.target.name] = e.target.files[0]
         this.setState({travelogu_info:curentSt})
@@ -71,30 +78,31 @@ class addTravelogu extends React.Component{
         <Form id="Form" onSubmit={this.handleSubmit}>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
         <Label id="form_label" className="mr-sm-2">متن سفرنامه</Label>
-        <Input name="text" value={this.state.travelogu_info.text} onChange={this.handleChangeNum} type="text" id="form_input"/>
+        <textarea  className="text_area_form" name="text" onChange={this.handleChange}  value={this.state.travelogu_info.text}> 
+        </textarea>
       </FormGroup>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-      <Label id="form_label">مکان ها</Label>
-          <Input type="select" onChange={this.updateState} value={this.state.travelogu_info.choosedPlace}  name="choosedPlace" id="form_input">
-           {this.state.places} 
-          </Input>
-    </FormGroup>
+        <Label id="form_label">مکان ها</Label>
+        <MultiSelect
+        items={this.state.places }
+        selectedItems={this.state.travelogu_info.choosedPlaces}
+        onChange={this.handleChangeMultiChoice}
+        />
+      </FormGroup>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label id="form_label">1بارگذاری عکس</Label>
-          <input onChange={this.updateStateImage} type="file" name="image1" id="form_input"/>
-        </FormGroup>
-        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-          <Label id="form_label">2بارگذاری عکس</Label>
-          <input onChange={this.updateStateImage} type="file" name="image2" id="form_input"/>
-        </FormGroup>
-        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-          <Label id="form_label">3بارگذاری عکس</Label>
-          <input onChange={this.updateStateImage}  type="file" name="image3" id="form_input"/>
-        </FormGroup>
+          <Input onChange={this.updateStateImage} type="file" name="image1" id="form_input"/>
+      </FormGroup>
+      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+        <Label id="form_label">2بارگذاری عکس</Label>
+        <Input onChange={this.updateStateImage} type="file" name="image2" id="form_input"/>
+      </FormGroup>
+      <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+        <Label id="form_label">3بارگذاری عکس</Label>
+        <Input onChange={this.updateStateImage}  type="file" name="image3" id="form_input"/>
+      </FormGroup>
       <Button id="placeform_submit">ثبت</Button>
-
     </Form>
-  
     );
 }
 }
