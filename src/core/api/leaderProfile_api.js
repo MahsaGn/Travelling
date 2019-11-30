3e181f3e8937a6af6908ccf57b8af3a098e49d8f
fatemerg <47068576+fatemerg@users.noreply.github.com>
2019-12-01 00:55:25 +0330

    add leader profile properties

--- src/core/api/leaderProfile_api.js
+++ src/core/api/leaderProfile_api.js
@@ -1,22 +1,28 @@
-import axios from 'axios'
-import store from '../../store.js'
-import {connect} from 'react-redux'
-class leaderProfile_api{
-static leaderProfile =async () => {
-    console.log("in api leaderProfile")
-    let usename = store.getState().leaderProfile_reducer.username
-    try{
-      let x = await axios.get(`http://localhost:8000/api/me/leader/${usename}`,{
-        headers:
-        {
-          'Accept' : 'application/json',
-          'Content-Type': 'application/json'
-      }})
-        console.log(x);  
-        return x.data
-        }catch{
-            console.log("wrong leaderProfile")
-            return false
-        }
-};
-}export default connect()(leaderProfile_api)
+import axios from "axios";
+import store from "../../store.js";
+import { connect } from "react-redux";
+class leaderProfile_api {
+  static leaderProfile = async () => {
+    console.log("in api leaderProfile");
+    let usename = store.getState().leaderProfile_reducer.username;
+    try {
+      let x = {
+        username: "fateme",
+        password: "fateme"
+        /*
+        email: "fateme",
+        first_name: "fateme",
+        last_name: "fateme",
+        itinerary: "fateme",
+        phone_number: "fateme",
+        avatar: "fateme"*/
+      };
+      console.log(x);
+      return x.data;
+    } catch {
+      console.log("wrong leaderProfile");
+      return false;
+    }
+  };
+}
+export default connect()(leaderProfile_api);
