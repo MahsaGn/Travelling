3033e1e766051b749984de6cb9f5d0a5796a1a1b
fatemerg <47068576+fatemerg@users.noreply.github.com>
2019-11-30 20:18:29 +0330

    fix leader card

--- src/Place/Tabs/leaderCard.js
+++ src/Place/Tabs/leaderCard.js
@@ -1,26 +1,43 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
-import '../../style.css';
-import { CardImg,CardBody, Card, Button, CardTitle } from 'reactstrap';
-import {Link} from 'react-router-dom'
-export default class NavItem_leader extends React.Component{
-    constructor(props){
-        super(props)
-        this.showProfile = this.showProfile.bind(this)
-    }
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "../../style.css";
+import { CardImg, CardBody, Card, Button, CardTitle } from "reactstrap";
+import { Link } from "react-router-dom";
+import LeaderCard from "./leaderCard";
+import Axios from "axios";
 
-    render(){
-        return(
-            <Card id="leader_card">
-            <CardImg top src={this.props.info.img} alt="Card image cap" />
-            <CardBody>
-              <CardTitle>{this.props.info.username}</CardTitle>
-              <Link to={{pathname: `/leaderProfile/${this.info.username}`,
-            data : {info: this.info.username}}}>
-                  <Button>نمایش جزئیات</Button>
-            </Link>
-            </CardBody>
-          </Card>
-        );
-    }
+export default class leaderCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.showProfile = this.showProfile.bind(this);
+    this.state = { isLeader: false };
+  }
+
+  componentWillMount() {
+    Axios.get("url").then(datas => {
+      this.setState({
+        leader: datas.map(d => <LeaderCard info={d} />),
+        isLeader: true
+      });
+    });
+  }
+
+  render() {
+    return (
+      <Card id="leader_card">
+        <CardImg top src={this.props.info.img} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>{this.props.info.username}</CardTitle>
+          <Link
+            to={{
+              pathname: `/leaderProfile/${this.info.username}`,
+              data: { info: this.info.username }
+            }}
+          >
+            <Button>نمایش جزئیات</Button>
+          </Link>
+        </CardBody>
+      </Card>
+    );
+  }
 }
