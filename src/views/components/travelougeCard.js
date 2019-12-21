13966be39eefd862baa383439deccfa7d8896032
mahsa <mahsa.ghaderan@gmail.com>
2019-12-22 00:15:00 +0330

    SpecificTravellouge

--- src/views/components/travelougeCard.js
+++ src/views/components/travelougeCard.js
@@ -10,7 +10,7 @@ export default class placeCard extends React.Component{
     }
 
     handleClick(){
-        window.location.replace(`/travelouge/${this.props.info.id}`)
+        window.location.replace(`/travellouge/${this.props.info.id}`)
     }
     
     render(){
