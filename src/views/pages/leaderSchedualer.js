dd296ce1b3b99b7bcf197904283b034b0a9d2d06
mahsa <mahsa.ghaderan@gmail.com>
2020-01-27 10:42:32 +0330

    Fix bug scheduler crrash

--- src/views/pages/leaderSchedualer.js
+++ src/views/pages/leaderSchedualer.js
@@ -1,7 +1,5 @@
 import * as React from "react";
-import axios from 'axios'
 import { connect } from 'react-redux'
-import * as ReactDOM from "react-dom";
 import * as userProfileAction from "../../core/userProfile/userProfile_action";
 
 import "../styles/App.css";
@@ -16,67 +14,58 @@ import "@syncfusion/ej2-lists/styles/material.css";
 import "@syncfusion/ej2-navigations/styles/material.css";
 import "@syncfusion/ej2-popups/styles/material.css";
 import { extend } from '@syncfusion/ej2-base';
-import userProfile_api from "../../core/api/userProfile_api"
 // import "@syncfusion/ej2-split-buttons/styles/material.css";
 import "@syncfusion/ej2-react-schedule/styles/material.css";
 import {
   ScheduleComponent,
-  WorkWeek,
   Week,
-  Month,
   Inject,
   ViewsDirective,
-  ViewDirective,
-  Day
+  ViewDirective
 } from "@syncfusion/ej2-react-schedule";
-import { isLeader_success } from "../../core/userProfile/userProfile_action";
 
-const scheduleData = [
-  {
-    Id: 2,
-    Subject: "Free",
-    StartTime: new Date(2018, 1, 15, 10, 0),
-    EndTime: new Date(2018, 1, 15, 12, 30),
-    IsAllDay: false,
-    Status: "Completed",
-    Priority: "High"
-  },
-  {
-    Id: 3,
-    Subject: "Free",
-    StartTime: new Date(2018, 1, 15, 11, 0),
-    EndTime: new Date(2018, 1, 15, 12, 30),
-    IsAllDay: false,
-    Status: "Completed",
-    Priority: "High"
-  }
-];
 
 class schedualer extends React.Component {
   constructor() {
     super(...arguments);
-    this.data = extend([], scheduleData, null, true);
+    this.data = extend([], "", null, true);
   }
   async onEventClick(args) {
-    console.log("in new event",this.data)
+    console.log("in new event", args)
     console.log(args)
     if (args.type == "DeleteAlert") {
       console.log("deleted")
         ;//remove data
     }
     else if (args.type == "QuickInfo") {
-      console.log("data is" ,this.data,"end")
-      if (this.props.scheduler_data=="" || !this.data.includes(args.data)) {
-        console.log("saved", this.data)
+      console.log("data is", this.data, "end")
+      if (this.props.scheduler_data == "" || !this.data.includes(args.data)) {
         await this.props.changeSchedluer(args.data)
       };//send data to back
+      if (this.data != "") {
+        console.log("saved", this.data)
+        this.data[this.data.length - 1].Subject = "Free"
+      }
     }
   }
 
   async componentWillMount() {
     await this.props.getCalenderData()
-    console.log("data is component will mount",this.data)
-    this.data = this.props.scheduler_data
+    console.log("data is component will mount", this.data)
+    let list = []
+    for (var i = 0; i < this.props.scheduler_data.length; i++) {
+      let objdata = this.props.scheduler_data[i]
+      let schedualerobj = {
+        Subject: "Free",
+        StartTime: objdata.StartTime,
+        EndTime: objdata.EndTime,
+        IsAllDay: false,
+        Status: "Completed",
+        Priority: "High"
+      }
+      list.push(schedualerobj)
+    }
+    this.data = list
   }
 
   render() {
@@ -112,7 +101,7 @@ const mapDispatchToProps = dispatch => {
   return {
     changeSchedluer: scheduler_data =>
       dispatch(userProfileAction.changeFreeTime(scheduler_data)),
-      getCalenderData: () =>
+    getCalenderData: () =>
       dispatch(userProfileAction.getCalenderData())
   };
 };
